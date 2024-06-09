ebf05ba218234cede319a54fd57a34e7233c9bdc
Shengyuan Ding <211250097@smail.nju.edu.cn>
2024-06-10 02:13:30 +0800

    feat: 修正prompt

--- agent_flask.py
+++ agent_flask.py
@@ -37,7 +37,7 @@ name = NAME
 passwd = quote_plus(password)
 uri = f"mysql+pymysql://{user}:{passwd}@{host}:3306/{name}"
 db = SQLDatabase.from_uri(uri)
-llm = Tongyi(model='qwen-max-longcontext', temperature=0)
+llm = Tongyi(model='qwen-max-0428', temperature=0)
 user_template = """
 请你根据以下问题、对应的SQL查询以及SQL查询结果，回答我的问题，如果不是特定的问题，你的回答要尽可能详细一点。
 问题: {question}
